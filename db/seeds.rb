8862a7f7880a2c50cdecdb30f1c2e9acfe5c7d60
Suriel Asael <surielasaelrosasmendez@MacBook-Pro-de-Suriel.local>
2019-01-04 13:04:45 -0600

    create users model and migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "suriel", password: "password")
+User.create(username: "juan", password: "password")
+User.create(username: "pedro", password: "password")
+User.create(username: "marco", password: "password")
+User.create(username: "mike", password: "password")
